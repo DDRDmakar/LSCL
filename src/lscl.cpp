1078b1dd8b38e81a3104f04a220f2ea9d4db7bcc
DDRDmakar <DDRDmakar@localhost.localdomain>
2019-05-01 02:54:53 +0300

    Parser basic structure

--- src/lscl.cpp
+++ src/lscl.cpp
@@ -3,16 +3,47 @@
 
 #include <string>
 #include <iostream>
+#include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 namespace LSCL
 {
 
+enum Node_type
+{
+	VOID, // Restricted to use
+	MAP,
+	LIST,
+	SCALAR
+};
+
 class Node
 {
-	
+public:
+	void free(void);
+	Node_type get_type(void);
+protected:
+	Node_type type;
 };
+
+class List : Node
+{
+private:
+	std::vector<Node> storage; // TODO reserve space in vector
+};
+
+class Scalar : Node
+{
+	std::string storage;
+};
+
+class Map : Node
+{
+	std::unordered_map<std::string, Node> storage;
+};
+
 
 class Nodebuilder
 {
@@ -20,7 +51,49 @@ public:
 	Nodebuilder(const std::string &input);
 	
 	Node build_node_from_text(const std::string &input);
+	
+	Map    build_map_from_text(const std::string &input, std::string::const_iterator &it);
+	List   build_list_from_text(void/*TODO*/);
+	Scalar build_scalar_from_text(/*TODO*/);
 };
+
+inline bool skip_spaces(const std::string &input, std::string::const_iterator &it)
+{
+	bool newline_skipped = false;
+	
+	while
+	(
+		it != input.cend() && 
+		(
+			*it == ' '    ||
+			*it == '\t'   ||
+			*it == '\x20' ||
+			*it == '\xA0'
+		)
+	)
+	{
+		++it;
+		if (it != input.cend() && *it == '\n') newline_skipped = true;
+	}
+	
+	return newline_skipped;
+}
+
+Map Nodebuilder::build_map_from_text(const std::string &input, std::string::const_iterator &it)
+{
+	unsigned int level = 0;
+	Map result;
+	
+	while (it != input.cend())
+	{
+		skip_spaces(input, it); // Here i have some symbol, which is not space.
+		if (!level && *it == '}') return result;
+		
+		
+	}
+	
+	return result;
+}
 
 Node Nodebuilder::build_node_from_text(const std::string &input)
 {
@@ -30,7 +103,7 @@ Node Nodebuilder::build_node_from_text(const std::string &input)
 	
 	size_t line_counter = 0;
 	
-	for (std::string::const_iterator it = input.begin(); it != input.end(); ++it)
+	for (std::string::const_iterator it = input.cbegin(); it != input.cend(); ++it)
 	{
 		++line_counter;
 		
